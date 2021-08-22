2f22598e721e819d1b932565e856b8600b5fb0f6
jwest <john.westbrook@rcsb.org>
2021-08-22 11:03:43 -0400

    Fix: fix linting issues

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ from setuptools import setup
 packages = []
 thisPackage = "rcsb.utils.validation"
 
-with open("rcsb/utils/validation/__init__.py", "r") as fd:
+with open("rcsb/utils/validation/__init__.py", "r", encoding="utf-8") as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
 
 if not version:
     raise RuntimeError("Cannot find version information")
 
 # Load packages from requirements*.txt
-with open("requirements.txt", "r") as ifh:
+with open("requirements.txt", "r", encoding="utf-8") as ifh:
     packagesRequired = [ln.strip() for ln in ifh.readlines()]
 
-with open("README.md", "r") as ifh:
+with open("README.md", "r", encoding="utf-8") as ifh:
     longDescription = ifh.read()
 
 
